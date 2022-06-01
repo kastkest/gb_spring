55154fb0ad444bb9c87cc242538a586ac010b5a9
kastkest <endimion-xi@yandex.ru>
2022-06-01 21:05:15 +0300

    Add servlet mapping with annotations

--- hw1/hw1/src/main/java/web_app_core/TestServlet.java
+++ hw1/hw1/src/main/java/web_app_core/TestServlet.java
@@ -4,16 +4,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name = "TestServlet", urlPatterns = "/product")
 public class TestServlet extends HttpServlet {
     private static Logger logger = LoggerFactory.getLogger(TestServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.info("Log: Get Product");
         resp.setContentType("text/html;charset=UTF-8");
         resp.getWriter().printf("<html><body>");
 
